52d14025f3e2114c8abb0d3d1fdcdf0cf1ddb645
shota-tsuji <shotatuzi@gmail.com>
2022-11-18 23:32:18 +0900

    feat: file is passed through arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+use std::env;
 use std::io::BufReader;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
     let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
     let sink = rodio::Sink::try_new(&handle).unwrap();
 
-    let file = std::fs::File::open("gs-16b-2c-44100hz.mp3").unwrap();
+    //let file = std::fs::File::open("Sample_BeeMoved_96kHz24bit.flac").unwrap();
+    let file = std::fs::File::open(&args[1]).unwrap();
     sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
 
     sink.sleep_until_end();
